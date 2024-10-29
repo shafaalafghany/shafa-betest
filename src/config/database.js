9975d7ad62655e0a143dccc38d937af5668490e6
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 03:38:57 +0700

    fix (config): fix error cnnect database mongo

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('database connected')
   } catch (e) {
     console.error('unable connect to database: ', e)
