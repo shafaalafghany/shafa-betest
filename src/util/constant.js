66137ea9376079c03c311dc57fd01c1efbf59dac
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 15:38:43 +0700

    refactor (util): add new constant for login

--- src/util/constant.js
+++ src/util/constant.js
@@ -2,6 +2,7 @@ module.exports = {
   COMMON_SERVER_ERROR: 'something happen on our server',
   EMAIL_ALREADY_USED: 'email already used',
   INVALID_PASSWORD: 'password must at least 8 characters',
+  INVALID_EMAIL_OR_PASSWORD: 'email or password is wrong',
 
   FAILED_CREATE_USER: 'failed to create user',
   FAILED_FIND_USER: 'failed to find user',
