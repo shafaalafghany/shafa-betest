fc26c1078489bfd0243748c5fdd1faafcbc51047
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 14:52:14 +0700

    feat (model): add user model

---
+++ src/model/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const { v4: uuidv4 } = require('uuid')
+
+const userSchema = new mongoose.Schema({
+  _id: { type: String, default: uuidv4 },
+  userName: { type: String, required: true },
+  accountNumber: { type: String, required: true, unique: true },
+  emailAddress: { type: String, required: true, unique: true },
+  identityNumber: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+})
+
+module.exports = mongoose.model('User', userSchema)
