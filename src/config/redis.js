73cbdec517286324199e302e8e100e3c75a29fbe
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 20:35:58 +0700

    refactor (config): change static string to dynamic based on env variable on socket host redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -6,7 +6,7 @@ const redisType = process.env.REDIS_TYPE || 'uri'
 if (redisType === 'uri') {
   redisClient = redis.createClient({ url: process.env.REDIS_URL })
 } else {
-  redisClient = redis.createClient({ socket: { host: 'localhost' } })
+  redisClient = redis.createClient({ socket: { host: process.env.REDIS_URL } })
 }
 
 redisClient.on('error', (error) => {
