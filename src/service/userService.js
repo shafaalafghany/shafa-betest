ae2a8ef760121f93850f99fcb95e1dc8fc5cbfb3
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 15:12:56 +0700

    feat (service): add service for user logic

---
+++ src/service/userService.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt')
+const constant = require('../util/constant')
+const { CustomError } = require('../util/errorHandler')
+
+class UserService {
+  constructor(userRepository) {
+    this.userRepository = userRepository
+  }
+
+  async register(data) {
+    try {
+      const emailExists = await this.userRepository.findUserByEmail(data.emailAddress)
+      console.log("AWAW", emailExists)
+      if (emailExists) throw new CustomError(constant.EMAIL_ALREADY_USED, 409)
+
+      const hashedPassword = await bcrypt.hash(data.password, 10)
+      data.password = hashedPassword
+      return await this.userRepository.createUser(data)
+    } catch (e) {
+      throw new CustomError(e.message || 'error register user', e.statusCode || 500)
+    }
+  }
+}
+
+module.exports = UserService
