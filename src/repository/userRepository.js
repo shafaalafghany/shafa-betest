43dd2cbac28594bc810b000fda31c083f97f930e
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 16:36:28 +0700

    feat (repository): add findUserById and UpdateUserById for user

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -19,13 +19,41 @@ class UserRepository {
     try {
       const cachedUser = await this.redis.get(`userEmail:${email}`)
       if (cachedUser) return JSON.parse(cachedUser)
-  
+
       const user = await this.userModel.findOne({ emailAddress: email })
       if (user) await this.redis.set(`userEmail:${email}`, JSON.stringify(user), 3600)
-      
+
       return user
     } catch (e) {
       throw new CustomError(constant.FAILED_FIND_USER)
+    }
+  }
+
+  async findUserById(id) {
+    try {
+      const cachedUser = await this.redis.get(`userId:${id}`)
+      if (cachedUser) return JSON.parse(cachedUser)
+
+      const user = await this.userModel.findOne({ _id: id }).select('-password')
+      if (user) await this.redis.set(`userid:${id}`, JSON.stringify(user), 3600)
+
+      return user
+    } catch (e) {
+      throw new CustomError(constant.FAILED_FIND_USER)
+    }
+  }
+
+  async updateUserById(data, id) {
+    try {
+      const user = await this.userModel.findByIdAndUpdate(id, { $set: data }, { new: true }).select('-password')
+      if (!user) throw new CustomError(constant.FAILED_UPDATE_USER)
+
+      await this.redis.del(`userId:${id}`)
+      await this.redis.del(`userEmail:${data.email}`)
+
+      return user
+    } catch (e) {
+      throw new CustomError(constant.FAILED_UPDATE_USER)
     }
   }
 }
