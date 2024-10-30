ecd89892be2ea93d82d41a3fa770d6f0e755a3ba
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 17:03:30 +0700

    refactor (model): add timestamp and deletedAt in user model

--- src/model/user.js
+++ src/model/user.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose')
 const { v4: uuidv4 } = require('uuid')
 
-const userSchema = new mongoose.Schema({
-  _id: { type: String, default: uuidv4 },
-  userName: { type: String, required: true },
-  accountNumber: { type: String, required: true, unique: true },
-  emailAddress: { type: String, required: true, unique: true },
-  identityNumber: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-})
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, default: uuidv4 },
+    userName: { type: String, required: true },
+    accountNumber: { type: String, required: true, unique: true },
+    emailAddress: { type: String, required: true, unique: true },
+    identityNumber: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    deletedAt: { type: Date, default: null },
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('User', userSchema)
