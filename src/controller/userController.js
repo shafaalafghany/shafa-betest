66c4739fdc99f4e4b3320466f20685d22665cdc4
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 15:39:40 +0700

    feat (controller): add login feature for user

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,11 +1,14 @@
 const constant = require('../util/constant')
+const { generateToken } = require('../util/jwt')
 const validator = require('../util/validator')
 const { SUCCESS } = require('../util/response')
 const { CustomError } = require('../util/errorHandler')
 
 const params = {
-  registerParams: ["userName", "accountNumber", "emailAddress", "identityNumber", "password"]
+  registerParams: ["userName", "accountNumber", "emailAddress", "identityNumber", "password"],
+  login: ["email", "password"],
 }
+
 
 class UserController {
   constructor(userService) {
@@ -34,9 +37,29 @@ class UserController {
         password,
       }
 
-      const user = await this.userService.register(payload)
+      await this.userService.register(payload)
 
       return SUCCESS(res, 201, constant.SUCCESS_REGISTER, { username: userName, email: emailAddress })
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+      const { email, password } = req.body
+
+      validator(req.body, params.login)
+
+      const user = await this.userService.login({email, password})
+      const token = generateToken(user)
+
+      const data = {
+        user,
+        token,
+      }
+
+      return SUCCESS(res, 200, '', data)
     } catch (e) {
       next(e)
     }
--- src/routes/v1.js
+++ src/routes/v1.js
@@ -5,5 +5,6 @@ const router = express.Router()
 const userController = Injector.userController()
 
 router.post('/auth', userController.register.bind(userController))
+router.post('/auth/login', userController.login.bind(userController))
 
 module.exports = router
