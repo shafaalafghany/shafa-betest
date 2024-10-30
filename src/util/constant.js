19ffe9dbda93ce87874e66051ce942fe05ea0b2c
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 16:36:00 +0700

    refactor (util): add constant that needed for middleware and update user

--- src/util/constant.js
+++ src/util/constant.js
@@ -3,8 +3,12 @@ module.exports = {
   EMAIL_ALREADY_USED: 'email already used',
   INVALID_PASSWORD: 'password must at least 8 characters',
   INVALID_EMAIL_OR_PASSWORD: 'email or password is wrong',
+  INVALID_TOKEN: 'invalid or expired token',
+  UNAUTHORIZED: 'unauthorization has been denied for this request',
 
   FAILED_CREATE_USER: 'failed to create user',
   FAILED_FIND_USER: 'failed to find user',
+  FAILED_UPDATE_USER: 'failed to update user',
+  USER_NOT_FOUND: 'user not found',
   SUCCESS_REGISTER: 'user registered',
 }
