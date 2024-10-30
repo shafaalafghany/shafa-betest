16514b36d18e2abea82f83c1a6c0be2e2b60f9fc
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 14:50:45 +0700

    feat (util): add constant global variable

---
+++ src/util/constant.js
@@ -0,0 +1,9 @@
+module.exports = {
+  COMMON_SERVER_ERROR: 'something happen on our server',
+  EMAIL_ALREADY_USED: 'email already used',
+  INVALID_PASSWORD: 'password must at least 8 characters',
+
+  FAILED_CREATE_USER: 'failed to create user',
+  FAILED_FIND_USER: 'failed to find user',
+  SUCCESS_REGISTER: 'user registered',
+}
