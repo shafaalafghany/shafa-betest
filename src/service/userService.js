a85d0b4d1e2531e8bcba4717e3852176f2ddc179
Shafa Alafghany <alafghanyshafa@gmail.com>
2024-10-30 16:37:15 +0700

    feat (service): add function update for user

--- src/service/userService.js
+++ src/service/userService.js
@@ -16,7 +16,7 @@ class UserService {
       data.password = hashedPassword
       return await this.userRepository.createUser(data)
     } catch (e) {
-      throw new CustomError(e.message || 'error register user', e.statusCode || 500)
+      throw new CustomError(e.message || constant.COMMON_SERVER_ERROR, e.statusCode || 500)
     }
   }
 
@@ -26,13 +26,34 @@ class UserService {
       if (!emailExists || !(await bcrypt.compare(data.password, emailExists.password))) throw new CustomError(constant.INVALID_EMAIL_OR_PASSWORD, 400)
 
       const user = emailExists instanceof this.userRepository.userModel
-                    ? emailExists.toJSON()
-                    : emailExists
+        ? emailExists.toJSON()
+        : emailExists
       delete user.password
 
       return user
     } catch (e) {
-      throw new CustomError(e.message || 'error register user', e.statusCode || 500)
+      throw new CustomError(e.message || constant.COMMON_SERVER_ERROR, e.statusCode || 500)
+    }
+  }
+
+  async update(data, id) {
+    try {
+      const payload = {
+        userName: data.username,
+        accountNumber: data.accountNumber,
+        identityNumber: data.identityNumber,
+      }
+
+      const user = await this.userRepository.findUserById(id)
+      if (!user) throw new CustomError(constant.USER_NOT_FOUND, 404)
+
+      const update = await this.userRepository.updateUserById(payload, id)
+      if (!update) throw new CustomError(constant.FAILED_UPDATE_USER, 500)
+
+      return update
+    } catch (e) {
+      console.log("ngehe", e)
+      throw new CustomError(e.message || constant.COMMON_SERVER_ERROR, e.statusCode || 500)
     }
   }
 }
